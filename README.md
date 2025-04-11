b29c8c3d725c9bb4f27a3eba431f55a464b36bed
Yue-bin <3103174284@qq.com>
2025-04-12 02:29:06 +0800

    🎉 init:

--- README.md
+++
@@ -1,2 +0,0 @@
-# Lunaqua
-A mod manager for stickfight:the game
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Lunaqua.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
